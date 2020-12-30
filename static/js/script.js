95f0419563ef9212715af3202229d89f4c092938
xander-webdev <xwernecke@gmail.com>
2020-12-30 15:19:35 +0000

    added categories drop down and changed javascript code to jquery for ease of use

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
-        edge: 'right'
-    }, {
-        preventScrolling: 'true'
-    }, {
-        draggable: 'true'
+$(document).ready(function () {
+    $(".sidenav").sidenav({
+        edge: "right",
+        draggable: "true",
+        preventScrolling: "true"
     });
-    //option was included in the init(elems,options) not sure what it was used for, this is a note incase errors spring up in the future
+    $(".collapsible").collapsible();
+    $("select").formSelect();
+    $('select').formSelect();
 });
