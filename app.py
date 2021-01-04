e0827c1e5c56c6fbbb749fd0cdafb64da64384bb
xander-webdev <xwernecke@gmail.com>
2021-01-04 18:04:14 +0000

    added smooth scroll to the project

--- app.py
+++ app.py
@@ -196,9 +196,10 @@ def edit_category(category_id):
 @app.route("/delete_category/<category_id>")
 def delete_category(category_id):
     mongo.db.categories.remove(
-        {"_id":ObjectId(category_id)})
+        {"_id": ObjectId(category_id)})
     flash("Category Successfully Deleted")
     return redirect(url_for("get_categories"))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
